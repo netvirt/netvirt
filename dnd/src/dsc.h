5903bbae354c007bbf9994f89c9cf584f3eaa2e1
nicboul@gmail.com <root@t61.(none)>
2012-03-04 14:18:14 -0500

    add PeerConnectInfo to DNDS protocol add post_handshake_check callback after SSL/TLS handshake

--- dnd/src/dsc.h
+++ dnd/src/dsc.h
@@ -1,6 +1,9 @@
 #ifndef DND_DSC_H
 #define DND_DSC_H
 
+#include <dnds/dnds.h>
+
+int transmit_peerconnectinfo(e_ConnectState state, char *ipAddress, char *certName);
 int dsc_init(char *address, char *port, char *certificate, char *priatekey, char *trusted_authority);
 
 #endif /* DND_DSC_H */
--- libdnds/src/asn1/AclGroup.c
+++ libdnds/src/asn1/AclGroup.c
@@ -420,10 +420,10 @@ static ber_tlv_tag_t asn_DEF_AclGroup_tags_1[] = {
 	(ASN_TAG_CLASS_UNIVERSAL | (16 << 2))
 };
 static asn_TYPE_tag2member_t asn_MAP_AclGroup_tag2el_1[] = {
-    { (ASN_TAG_CLASS_CONTEXT | (0 << 2)), 0, 0, 0 }, /* id at 149 */
-    { (ASN_TAG_CLASS_CONTEXT | (1 << 2)), 1, 0, 0 }, /* contextId at 150 */
-    { (ASN_TAG_CLASS_CONTEXT | (2 << 2)), 2, 0, 0 }, /* name at 151 */
-    { (ASN_TAG_CLASS_CONTEXT | (3 << 2)), 3, 0, 0 } /* description at 152 */
+    { (ASN_TAG_CLASS_CONTEXT | (0 << 2)), 0, 0, 0 }, /* id at 163 */
+    { (ASN_TAG_CLASS_CONTEXT | (1 << 2)), 1, 0, 0 }, /* contextId at 164 */
+    { (ASN_TAG_CLASS_CONTEXT | (2 << 2)), 2, 0, 0 }, /* name at 165 */
+    { (ASN_TAG_CLASS_CONTEXT | (3 << 2)), 3, 0, 0 } /* description at 166 */
 };
 static asn_SEQUENCE_specifics_t asn_SPC_AclGroup_specs_1 = {
 	sizeof(struct AclGroup),
