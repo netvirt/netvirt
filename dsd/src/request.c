2b725d3325643b9f12d1e721490605671294c465
Nicolas Bouliane <nib@dynvpn.com>
2012-04-12 20:59:26 -0400

    Remove ContextInfo message and use and extend SearchRequest/SearchResponse to transmit context information from DSD to DND
    DND now transmit the client certificate Name to DSD via the peerConnectInfo message

--- dsd/src/request.c
+++ dsd/src/request.c
@@ -10,6 +10,8 @@
  */
 
 #include <dnds/dnds.h>
+
+#include "dao.h"
 #include "request.h"
 
 void peerConnectInfo(struct session *session, DNDSMessage_t *req_msg)
@@ -61,7 +63,55 @@ void modifyRequest(struct session *session, DNDSMessage_t *msg)
 
 }
 
-void searchRequest(struct session *session, DNDSMessage_t *msg)
+/* XXX This is a prototype, it only handle context object,
+ * with a request type set to all. Will be expanded in near future...
+ */
+void searchRequest(struct session *session, DNDSMessage_t *req_msg)
 {
+	char *id;
+	char *topology_id;
+	char *description;
+	char *network;
+	char *netmask;
+	char *serverCert;
+	char *serverPrivkey;
+	char *trustedCert;
 
+	dao_fetch_context(&id,
+			&topology_id,
+			&description,
+			&network,
+			&netmask,
+			&serverCert,
+			&serverPrivkey,
+			&trustedCert);
+
+        DNDSMessage_t *msg;
+
+        DNDSMessage_new(&msg);
+        DNDSMessage_set_channel(msg, 0);
+        DNDSMessage_set_pdu(msg, pdu_PR_dsm);
+
+        DSMessage_set_seqNumber(msg, 0);
+        DSMessage_set_ackNumber(msg, 1);
+        DSMessage_set_operation(msg, dsop_PR_searchResponse);
+
+        SearchResponse_set_result(msg, DNDSResult_success);
+
+        DNDSObject_t *objContext;
+        DNDSObject_new(&objContext);
+        DNDSObject_set_objectType(objContext, DNDSObject_PR_context);
+
+        Context_set_id(objContext, atoi(id));
+        Context_set_topology(objContext, Topology_mesh);
+        Context_set_description(objContext, description, strlen(description));
+        Context_set_network(objContext, network);
+        Context_set_netmask(objContext, netmask);
+        Context_set_serverCert(objContext, serverCert, strlen(serverCert));
+        Context_set_serverPrivkey(objContext, serverPrivkey, strlen(serverPrivkey));
+        Context_set_trustedCert(objContext, trustedCert, strlen(trustedCert));
+
+        SearchResponse_add_object(msg, objContext);
+
+	net_send_msg(session->netc, msg);
 }
