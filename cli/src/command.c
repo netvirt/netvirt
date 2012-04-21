79d3b7f75be5b57ea4a7f01fbbe214f530ef9fe3
Jamael Seun <jamael.seun@gmail.com>
2012-04-21 12:22:17 -0400

    Change copyright holder to Jamael Seun

--- cli/src/command.c
+++ cli/src/command.c
@@ -1,6 +1,6 @@
 /*
  * command.c: Command prompt
- * Copyright 2012. The OpenDNDS team. <team@opendnds.org>
+ * Copyright 2012. Jamael Seun
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
