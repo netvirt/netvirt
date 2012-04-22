043d7c29f9c46224434ad0bb9a8745d5f3a6e147
Jamael Seun <jamael.seun@gmail.com>
2012-04-22 09:57:15 -0400

    DNC connection retry (the ugly way)
    Using a loop inside on_disconnect() and hardcoding magic value of 5 seconds to sleep(), this patch allows DNC to try reconnecting forever.
    
    Note: compile but untested.

--- dnc/src/dnc.c
+++ dnc/src/dnc.c
@@ -144,12 +144,37 @@ void transmit_register(netc_t *netc)
         return;
 }
 
+static void on_input(netc_t *netc);
+static void on_secure(netc_t *netc);
+
 static void on_disconnect(netc_t *netc)
 {
 	printf("on_disconnect !!\n");
-
+	netc_t *retry_netc = NULL;
 	struct session *session;
+
 	session = netc->ext_ptr;
+	session->status = SESSION_STATUS_DOWN;
+	
+	do {
+		sleep(5);
+		printf("connection retry... ");
+		retry_netc = net_client(session->server_address,
+		    session->server_port, NET_PROTO_UDT, NET_SECURE_ADH,
+		    session->passport, on_disconnect, on_input, on_secure);
+
+		if (retry_netc)
+			break;
+		
+		printf("failed\n");
+	} while (1);
+
+	printf("ok\n");
+	session->status = SESSION_STATUS_NOT_AUTHED;
+	session->netc = retry_netc;
+	retry_netc->ext_ptr = session;
+
+	free(netc);
 }
 
 /* only used by P2P */
@@ -308,6 +333,8 @@ int dnc_init(char *server_address, char *server_port,
 	session->passport = pki_passport_load_from_file(certificate,
 							 privatekey,
 							 trusted_authority);
+	session->server_address = server_address;
+	session->server_port = server_port;
 
 	netc = net_client(server_address, server_port, NET_PROTO_UDT, NET_SECURE_ADH,
 		session->passport, on_disconnect, on_input, on_secure);
