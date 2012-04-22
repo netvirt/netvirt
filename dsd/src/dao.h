0054054e5b15d3a61d673e26b7bc5729f72fd6e1
Nicolas Bouliane <nicboul@gmail.com>
2012-04-22 11:02:48 -0400

    add dao subsystem

---
+++ dsd/src/dao.h
@@ -0,0 +1,24 @@
+/*
+ * Dynamic Network Directory Service
+ * Copyright (C) 2010-2012 Nicolas Bouliane
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ */
+
+#ifndef DSD_DAO_H
+#define DSD_DAO_h
+
+int dao_connect(char *host, char *username, char *password, char *dbname);
+int dao_fetch_context(char **id,
+			char **topology_id,
+			char **description,
+			char **network,
+			char **netmask,
+			char **serverCert,
+			char **serverPrivkey,
+			char **trustedCert);
+#endif
