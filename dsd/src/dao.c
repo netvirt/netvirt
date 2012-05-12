d7110e7b7f53a19672cc069747cfe5c89baa8f70
Nicolas Bouliane <nib@dynvpn.com>
2012-05-12 09:47:39 -0400

    fix the add client sql syntax

--- dsd/src/dao.c
+++ dsd/src/dao.c
@@ -56,12 +56,34 @@ int dao_add_client(char *firstname,
 	PGresult *result;
 	char insert_req[512];
 
-	snprintf(insert_req,	"INSERT INTO client"
-				"(firstname, lastname, email, company, phone, country, state_province, city, postal_code, status)"
-				"VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, 1);",
+	snprintf(insert_req, 512, "INSERT INTO client "
+				"(firstname, lastname, email, company, phone, country, state_province, city, postal_code, status) "
+				"VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', 1);",
 				firstname, lastname, email, company, phone, country, state_province, city, postal_code);
 
+	printf("insert_req: %s\n", insert_req);
+
 	result = PQexec(dbconn, insert_req);
+
+	if (!result) {
+		printf("PQexec command failed, no error code\n");
+	}
+
+	switch (PQresultStatus(result)) {
+	case PGRES_COMMAND_OK:
+		printf("command executed ok, %s rows affected\n", PQcmdTuples(result));
+		break;
+	case PGRES_TUPLES_OK:
+		printf("query may have returned data\n");
+		break;
+	default:
+		printf("command failed with code %s, error message %s\n",
+			PQresStatus(PQresultStatus(result)),
+			PQresultErrorMessage(result));
+		break;
+	}
+
+	return 0;
 }
 
 int dao_fetch_context(char **id,
@@ -106,12 +128,12 @@ int dao_fetch_context(char **id,
 
 	printf("Tuples %d\n", tuples);
 	printf("Fields %d\n", fields);
-/*
+
 	int i;
 	for (i = 0; i<fields; i++) {
 		printf("%s | %s\n", PQfname(result, i), PQgetvalue(result, 0, i));
 	}
-*/
+
 	*id = strdup(PQgetvalue(result, 0, 0));
 	*topology_id = strdup(PQgetvalue(result, 0, 1));
 	*description = strdup(PQgetvalue(result, 0, 2));
@@ -124,10 +146,21 @@ int dao_fetch_context(char **id,
 	return 0;
 }
 
-/*
-int main()
+#if 0
+int main(int argc, char *argv[])
 {
-	dao_connect();
-	dao_fetch_context();
+	dao_connect(argv[1], argv[2], argv[3], argv[4]);
+//	dao_fetch_context();
+
+	dao_add_client("firstname",
+			"lastname",
+			"email",
+			"company",
+			"phone",
+			"country",
+			"state_province",
+			"city",
+			"postal_code");
+
 }
-*/
+#endif
