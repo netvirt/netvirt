8c52fba4b3baf32c636789c83da0f7e4dd1d530b
Nicolas Bouliane <nib@dynvpn.com>
2012-05-20 12:02:13 -0400

    add more DAO
    dao_update_embassy_issue_serial
    dao_fetch_embassy
    
    and some code to test it...

--- dsd/src/dao.c
+++ dsd/src/dao.c
@@ -403,6 +403,80 @@ int dao_fetch_client_id(char **client_id, char *firstname, char *lastname, char 
 
 }
 
+int dao_update_embassy_issue_serial(char *context_id, uint32_t issue_serial)
+{
+	PGresult *result;
+	char update_req[256];
+
+	snprintf(update_req, 256, "UPDATE embassy "
+				"SET issue_serial = '%d' "
+				"WHERE context_id = '%s';",
+				issue_serial, context_id);
+
+	printf("update_req: %s\n", update_req);
+
+	result = PQexec(dbconn, update_req);
+
+	if (!result) {
+		printf("PQexec command failed, no error code\n");
+	}
+
+	switch (PQresultStatus(result)) {
+	case PGRES_COMMAND_OK:
+		printf("command executed ok, %s rows affected\n", PQcmdTuples(result));
+		break;
+	case PGRES_TUPLES_OK:
+		printf("query may have returned data\n");
+		break;
+	default:
+		printf("command failed with code %s, error message %s\n",
+			PQresStatus(PQresultStatus(result)),
+			PQresultErrorMessage(result));
+		break;
+	}
+
+}
+
+
+int dao_fetch_embassy(char *context_id,
+			char **certificate,
+			char **private_key,
+			char **issue_serial)
+{
+	PGresult *result;
+	char fetch_req[512];
+
+	snprintf(fetch_req, 512, "SELECT certificate, private_key, issue_serial "
+				"FROM EMBASSY "
+				"WHERE context_id = '%s';",
+				context_id);
+
+	printf("fetch_req: %s\n", fetch_req);
+
+	result = PQexec(dbconn, fetch_req);
+	*certificate = strdup(PQgetvalue(result, 0, 0));
+	*private_key = strdup(PQgetvalue(result, 0, 1));
+	*issue_serial = strdup(PQgetvalue(result, 0, 2));
+
+	if (!result) {
+		printf("PQexec command failed, no error code\n");
+	}
+
+	switch (PQresultStatus(result)) {
+	case PGRES_COMMAND_OK:
+		printf("command executed ok, %s rows affected\n", PQcmdTuples(result));
+		break;
+	case PGRES_TUPLES_OK:
+		printf("query may have returned data\n");
+		break;
+	default:
+		printf("command failed with code %s, error message %s\n",
+			PQresStatus(PQresultStatus(result)),
+			PQresultErrorMessage(result));
+		break;
+	}
+}
+
 int dao_fetch_context(char **id,
 			char **topology_id,
 			char **description,
@@ -536,8 +610,15 @@ int main(int argc, char *argv[])
 
 	/* DNC certificate */
 
-	char *uuid;
+	// fetch embassy
+	char *certificate, *private_key, *serial;
+	embassy_t *f_emb;
+	dao_fetch_embassy(context_id, &certificate, &private_key, &serial);
 
+	f_emb = pki_embassy_load_from_memory(certificate, private_key, atoi(serial));
+
+
+	char *uuid;
 	uuid = uuid_v4();
 
 	char common_name[256];
@@ -548,12 +629,14 @@ int main(int argc, char *argv[])
 	dnc_id = pki_digital_id(common_name, "CA", "Quebec", "Levis", "info@demo.com", "DNDS");
 
 	passport_t *dnc_passport;
-	dnc_passport = pki_embassy_deliver_passport(emb, dnc_id, exp_delay);
+	dnc_passport = pki_embassy_deliver_passport(f_emb, dnc_id, exp_delay);
 
 	pki_write_certificate_in_mem(dnc_passport->certificate, &cert_ptr, &size);
 	pki_write_privatekey_in_mem(dnc_passport->keyring, &pvkey_ptr, &size);
 
 	dao_add_passport_client(context_id, uuid, cert_ptr, pvkey_ptr);
+
+	dao_update_embassy_issue_serial(context_id, f_emb->serial);
 
 	free(cert_ptr);
 	free(pvkey_ptr);
