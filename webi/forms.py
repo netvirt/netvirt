c19c29c17d9b0426a5f762f376e3204e2ee39667
nicboul <nicboul@gmail.com>
2013-01-04 21:11:45 -0500

    add many forms to register/login, to context/nodes, and to dashboard use bootstrap to perform css/html presentation

--- webi/forms.py
+++ webi/forms.py
@@ -1,13 +1,66 @@
 from django import forms
 from django_countries.countries import COUNTRIES
+from captcha.fields import CaptchaField
 
-class ClientNewForm(forms.Form):
-    firstname = forms.CharField(required=True)
-    lastname = forms.CharField(required=True)
-    country = forms.ChoiceField(COUNTRIES, required=True, initial='CA')
-    email = forms.EmailField(required=True)
-    passwd = forms.CharField(required=True, label="Password")
+class ClientRegisterForm(forms.Form):
+    firstname = forms.CharField(required=True, label="")
+    firstname.widget.attrs['class'] = 'form-signin'
+    firstname.widget.attrs['type'] = 'text'
+    firstname.widget.attrs['placeholder'] = 'Firstname'
+
+    lastname = forms.CharField(required=True, label="")
+    lastname.widget.attrs['class'] = 'form-signin'
+    lastname.widget.attrs['type'] = 'text'
+    lastname.widget.attrs['placeholder'] = 'Lastname'
+
+    country = forms.ChoiceField(COUNTRIES, required=True, initial='CA', label="")
+
+    email = forms.EmailField(required=True, label="")
+    email.widget.attrs['class'] = 'form-signin'
+    email.widget.attrs['type'] = 'text'
+    email.widget.attrs['placeholder'] = 'Email address'
+
+    passwd = forms.CharField(required=True, label="")
+    passwd.widget.attrs['class'] = 'form-signin'
+    passwd.widget.attrs['type'] = 'password'
+    passwd.widget.attrs['placeholder'] = 'Password'
+
+    captcha = CaptchaField(required=True, label="")
+    captcha.widget.attrs['class'] = 'form-signin'
+    captcha.widget.attrs['placeholder'] = 'Captcha'
 
 class ClientLoginForm(forms.Form):
-    email = forms.EmailField(required=True)
-    passwd = forms.CharField(required=True, label="Password")
+    email = forms.EmailField(required=True, label="")
+    email.widget.attrs['class'] = 'form-signin'
+    email.widget.attrs['type'] = 'text'
+    email.widget.attrs['placeholder'] = 'Email address'
+
+    passwd = forms.CharField(required=True, label="")
+    passwd.widget.attrs['class'] = 'form-signin'
+    passwd.widget.attrs['type'] = 'password'
+    passwd.widget.attrs['placeholder'] = 'Password'
+
+    captcha = CaptchaField(required=True, label="")
+    captcha.widget.attrs['class'] = 'form-signin'
+    captcha.widget.attrs['placeholder'] = 'Captcha'
+
+
+class ContextAddForm(forms.Form):
+    desc = forms.CharField(required=True, label="")
+    desc.widget.attrs['class'] = 'form-signin'
+    desc.widget.attrs['type'] = 'text'
+    desc.widget.attrs['placeholder'] = 'Description'
+
+    captcha = CaptchaField(required=True, label="")
+    captcha.widget.attrs['class'] = 'form-signin'
+    captcha.widget.attrs['placeholder'] = 'Captcha'
+
+class NodeAddForm(forms.Form):
+    desc = forms.CharField(required=True, label="")
+    desc.widget.attrs['class'] = 'form-signin'
+    desc.widget.attrs['type'] = 'text'
+    desc.widget.attrs['placeholder'] = 'Description'
+
+    captcha = CaptchaField(required=True, label="")
+    captcha.widget.attrs['class'] = 'form-signin'
+    captcha.widget.attrs['placeholder'] = 'Captcha'
--- webi/views.py
+++ webi/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from dnds.forms import ClientNewForm, ClientLoginForm
+from dnds.forms import *
 from dsctl import *
 from time import sleep
 
@@ -8,10 +8,14 @@ def welcome(request):
     html = "<html><body> Welcome ! </body></html>"
     return HttpResponse(html)
 
-def client_new(request):
+def client_register(request):
+
     if request.method == 'POST':
-        form = ClientNewForm(request.POST)
+
+        form = ClientRegisterForm(request.POST)
+
         if form.is_valid():
+
             cd = form.cleaned_data
             strclient = cd['firstname']+","+cd['lastname']+","+cd['email']+","+cd['passwd']+",none,none,"+cd['country']+",none,none,none"
 
@@ -23,24 +27,36 @@ def client_new(request):
 
             return HttpResponse(strclient)
     else:
-        form = ClientNewForm()
-    return render(request, 'client_new_form.html', {'form': form})
+        form = ClientRegisterForm()
+    return render(request, 'client_register_form.html', {'form': form})
 
 def client_login(request):
+
+    try:
+        if request.session['logged'] == True:
+            return HttpResponseRedirect("/dnds/client/logoff/")
+    except KeyError:
+        pass
+
     if request.method == 'POST':
+
         form = ClientLoginForm(request.POST)
+
         if form.is_valid():
+
             cd = form.cleaned_data
             strlogin = cd['email'] + "," + cd['passwd']
-
 
             conn = Connection()
             connect(conn, '127.0.0.1')
             login(conn, strlogin)
+            request.session['client_id'] = conn.ClientId
             sleep(2)
+            disconnect(conn)
 
             if conn.loggedin == True:
-                strlogin = strlogin + " -> Logged in !"
+                request.session['logged'] = True
+                return HttpResponseRedirect("/dnds/client/dashboard/")
             else:
                 strlogin = strlogin + " -> Logging failed !"
 
@@ -48,4 +64,100 @@ def client_login(request):
 
     form = ClientLoginForm
     return render(request, 'client_login_form.html', {'form': form})
+
+def client_logoff(request):
+
+    try:
+        del request.session['logged']
+    except KeyError:
+        pass
+
+    return HttpResponseRedirect("/dnds/client/login/")
+
+class Context:
+    cid = ""
+    desc = ""
+    nodes = []
+
+def client_dashboard(request):
+
+    logged = request.session.get('logged', '')
+    if logged != True:
+            return HttpResponseRedirect("/dnds/client/login/")
+
+    conn = Connection()
+    connect(conn, '127.0.0.1')
+    conn.ClientId = request.session.get('client_id', '0')
+    t = showContext(conn)
+    sleep(1)
+
+    ctx_list = []
+    for d in t:
+        ctx = Context()
+        ctx.cid = str(d['id'])
+        ctx.desc = str(d['desc'])
+        ctx.nodes = showNode(conn, str(d['id']))
+        ctx_list.append(ctx)
+        sleep(1)
+
+    disconnect(conn)
+
+    return render(request, 'client_dashboard.html',
+        {'client_id': conn.ClientId, 'context': ctx_list})
+
+def context_add(request):
+
+    logged = request.session.get('logged', '')
+    if logged != True:
+            return HttpResponseRedirect("/dnds/client/login/")
+
+    if request.method == 'POST':
+
+        form = ContextAddForm(request.POST)
+
+        if form.is_valid():
+
+            cd = form.cleaned_data
+            conn = Connection()
+            connect(conn, '127.0.0.1')
+            conn.ClientId = request.session.get('client_id', '0')
+            addContext(conn, cd['desc'])
+            sleep(2)
+            disconnect(conn)
+
+            return HttpResponseRedirect("/dnds/client/dashboard/")
+
+    else:
+        form = ContextAddForm()
+
+    return render(request, 'context_add_form.html', {'form': form})
+
+
+def node_add(request, cid):
+
+    logged = request.session.get('logged', '')
+    if logged != True:
+            return HttpResponseRedirect("/dnds/client/login/")
+
+    if request.method == 'POST':
+
+        form = NodeAddForm(request.POST)
+
+        if form.is_valid():
+
+            cd = form.cleaned_data
+            conn = Connection()
+            connect(conn, '127.0.0.1')
+            conn.ClientId = request.session.get('client_id', '0')
+            addNode(conn, cid, cd['desc'])
+            sleep(1)
+            disconnect(conn)
+
+            return HttpResponseRedirect("/dnds/client/dashboard/")
+
+    else:
+        form = ContextAddForm()
+
+    return render(request, 'node_add_form.html', {'form': form})
+
 
