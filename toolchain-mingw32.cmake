5246218e0a525995d7949874c19f6ebd854eab56
nicboul <nicboul@gmail.com>
2013-06-08 17:48:51 -0400

    add cmake toolchain insctructions to cross compile with mingw32

---
+++ toolchain-mingw32.cmake
@@ -0,0 +1,10 @@
+set(CMAKE_SYSTEM_NAME "Windows")
+set(CMAKE_C_COMPILER "i686-w64-mingw32-gcc")
+set(CMAKE_CXX_COMPILER "i686-w64-mingw32-g++")
+set(CMAKE_RC_COMPILER "i686-w64-mingw32-windres")
+
+set(CMAKE_FIND_ROOT_PATH "/opt/mingw32/mingw32")
+
+include_directories(/opt/mingw32/i686-w64-mingw32/include)
+
+
