de59bcfc99e6bdb29ebd277f73daeb0a52839612
Sebastien Duthil <duthils@free.fr>
2013-06-29 22:48:20 -0400

    improve instructions for cross-compilation

--- toolchain-mingw32.cmake
+++ toolchain-mingw32.cmake
@@ -1,10 +1,4 @@
 set(CMAKE_SYSTEM_NAME "Windows")
-set(CMAKE_C_COMPILER "i686-w64-mingw32-gcc")
-set(CMAKE_CXX_COMPILER "i686-w64-mingw32-g++")
-set(CMAKE_RC_COMPILER "i686-w64-mingw32-windres")
-
-set(CMAKE_FIND_ROOT_PATH "/opt/mingw32/mingw32")
-
-include_directories(/opt/mingw32/i686-w64-mingw32/include)
-
-
+set(CMAKE_C_COMPILER "${CROSS_COMPILER}-gcc")
+set(CMAKE_CXX_COMPILER "${CROSS_COMPILER}-g++")
+set(CMAKE_RC_COMPILER "${CROSS_COMPILER}-windres")
