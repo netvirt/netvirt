cc4c110c9ba1d3e89d915343a6042341053c40c7
nicboul <nicboul@gmail.com>
2013-10-28 18:40:07 -0400

    Windows tun/tap driver for 32 and 64bit

---
+++ tap-driver-32_64/32-bit/addtap.bat
@@ -0,0 +1,3 @@
+rem Add a new TAP-Win32 virtual ethernet adapter
+tapinstall.exe install OemWin2k.inf tap0901
+pause
---
+++ tap-driver-32_64/64-bit/deltapall.bat
@@ -0,0 +1,4 @@
+echo WARNING: this script will delete ALL TAP-Win32 virtual adapters (use the device manager to delete adapters one at a time)
+pause
+tapinstall.exe remove tap0901
+pause
