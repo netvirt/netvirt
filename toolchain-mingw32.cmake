c35d30fadc839bde9dfa14f64a16bce1bd361327
Nicolas J. Bouliane <nicboul@gmail.com>
2014-03-04 21:08:28 -0600

    these are only example

--- toolchain-mingw32.cmake
+++ toolchain-mingw32.cmake
@@ -1,9 +1,9 @@
 set(CMAKE_SYSTEM_NAME "Windows")
 
 # You might need to edit that section
-set(CROSS_COMPILER "i686-w64-mingw32")
-set(OPENSSL_ROOT_DIR "/opt/mingw32/mingw32")
-set(CMAKE_FIND_ROOT_PATH "/media/nib/Windows7_OS/Qt/4.8.4")
+#set(CROSS_COMPILER "i686-w64-mingw32")
+#set(OPENSSL_ROOT_DIR "/opt/mingw32/mingw32")
+#set(CMAKE_FIND_ROOT_PATH "/media/nib/Windows7_OS/Qt/4.8.4")
 
 set(CMAKE_C_COMPILER "${CROSS_COMPILER}-gcc")
 set(CMAKE_CXX_COMPILER "${CROSS_COMPILER}-g++")
