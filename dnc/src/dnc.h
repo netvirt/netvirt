e795dc4ac31558d68850976ec31168063ebe1c08
Nicolas J. Bouliane <nicboul@gmail.com>
2014-03-18 10:51:40 -0600

    update DNC license to GPLv3

--- dnc/src/dnc.h
+++ dnc/src/dnc.h
@@ -1,13 +1,17 @@
 /*
  * Dynamic Network Directory Service
- * Copyright (C) 2009-2013
+ * Copyright (C) 2009-2014
  * Nicolas J. Bouliane <nib@dynvpn.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; version 2
+ * as published by the Free Software Foundation; version 3
  * of the License.
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 #ifndef DNC_H
--- dnc/src/gui/wizarddialog.h
+++ dnc/src/gui/wizarddialog.h
@@ -5,9 +5,13 @@
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; version 2
+ * as published by the Free Software Foundation; version 3
  * of the License.
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 #ifndef WIZARDDIALOG_H
