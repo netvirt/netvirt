4ef9341c7d7977e3ed79fadb5f6254d0708e628a
Nicolas J. Bouliane <nicboul@gmail.com>
2016-08-06 15:46:53 +0000

    new agent skeleton

--- nvagent/src/cli/main.c
+++ nvagent/src/cli/main.c
@@ -14,11 +14,47 @@
  * GNU General Public License for more details.
  */
 
+#include <err.h>
+#include <signal.h>
+
+#include <event.h>
+
 #include "../agent.h"
+
+struct event_base	*ev_base;
+
+void
+sighandler(int signal, short events, void *arg)
+{
+	(void)signal;
+	(void)events;
+
+	event_base_loopbreak(arg);
+}
 
 int
 main()
 {
+	struct event	ev_sigint;
+	struct event	ev_sigterm;
+
+	if ((ev_base = event_init()) == NULL)
+		errx(1, "event_base_new");
+
+	signal_set(&ev_sigint, SIGINT, sighandler, ev_base);
+	if (signal_add(&ev_sigint, NULL) < 0)
+		errx(1, "signal_add");
+
+	signal_set(&ev_sigterm, SIGTERM, sighandler, ev_base);
+	if (signal_add(&ev_sigterm, NULL) < 0)
+		errx(1, "signal_add");
+
+	agent_init();
+
+	event_base_dispatch(ev_base);
+
+	agent_fini();
+	event_base_free(ev_base);
 
 	return (0);
 }
