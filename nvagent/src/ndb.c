cfc4a5038074850bfdc890e28f259cfb79e32a7a
Nicolas J. Bouliane <nicboul@gmail.com>
2017-05-13 13:31:49 -0400

    ndb: add load file and save file

--- nvagent/src/ndb.c
+++ nvagent/src/ndb.c
@@ -14,24 +14,42 @@
  */
 
 #include <sys/stat.h>
+#include <sys/tree.h>
 
 #include <string.h>
 
 #include <jansson.h>
 
-static int	ndb_fullpath(const char *, char *);
+#include "agent.h"
+
+#define NDB_VERSION 1
 
 struct network {
-	char	*name;
-	char	*cert;
-	char	*pvkey;
-	char	*cacert;
+	RB_ENTRY(network)	 entry;
+	size_t			 idx;
+	char			*name;
+	char			*cert;
+	char			*pvkey;
+	char			*cacert;
 };
 
-struct networkdb {
-	int	 version;
+RB_HEAD(network_tree, network);
 
-};
+struct network_tree	 networks;
+json_t			*jdb;
+json_t			*jnetworks;
+int			 version;
+char			 ndb_path[256];
+
+static int	ndb_fullpath(const char *, char *);
+static int	network_cmp(const struct network *, const struct network *);
+RB_PROTOTYPE_STATIC(network_tree, network, entry, network_cmp);
+
+int
+network_cmp(const struct network *a, const struct network *b)
+{
+	return strcmp(a->name, b->name);
+}
 
 #if defined(__unix__) && !defined(__APPLE__)
 static int
@@ -71,17 +89,17 @@ ndb_fullpath(const char *file, char *fullname)
 int
 ndb_init(void)
 {
-	json_t		*jdb;
-	json_t		*jnetworks;
 	json_t		*jnetwork;
 	json_error_t	 error;
+	struct network	 *n;
+	int		 version;
 	size_t		 array_size;
 	size_t		 i;
 	char		 path[256];
 
 #if defined(__unix__)
 	{
-		/* Create ~/.config/netvirt if it doesn't exist. */
+		/* Create ~/.config/netvirt/ if it doesn't exist. */
 		struct	 stat st;
 
 		if (ndb_fullpath("", path) < 0) {
@@ -99,35 +117,134 @@ ndb_init(void)
 	}
 #endif
 
-	if (ndb_fullpath("nvswitch.nv", path) < 0) {
+	if (ndb_fullpath("nvswitch.nv", ndb_path) < 0) {
 		fprintf(stderr, "%s: ndb_fullpath\n", __func__);
 		exit(-1);
 	}
 
-	jdb = json_load_file(path, 0, &error);
+	if ((jdb = json_load_file(path, 0, &error)) == NULL)
+		return (0);
 
-	struct network n;
-	struct networkdb ndb;
-
-	json_unpack(jdb, "{s:i}", "version", &ndb.version);
-	printf("version %d\n", ndb.version);
-
-	jnetworks = json_object_get(jdb, "networks");
-
-	array_size = json_array_size(jnetworks);
-
-	for (i = 0; i < array_size; i++) {
-
-		jnetwork = json_array_get(jnetworks, i);
-
-		json_unpack(jnetwork, "{s:s, s:s, s:s, s:s}", "name", &n.name,
-		    "cert", &n.cert, "pvkey", &n.pvkey, "cacert", &n.cacert);
-
-		printf("cert:\n%s\n", n.cert);
+	if ((json_unpack(jdb, "{s:i}", "version", &version)) == -1) {
+		fprintf(stderr, "%s: json_unpack\n", __func__);
+		return (-1);
 	}
 
+	if ((jnetworks = json_object_get(jdb, "networks")) == NULL)
+		return (0);
+
+	array_size = json_array_size(jnetworks);
+	for (i = 0; i < array_size; i++) {
+
+		if ((jnetwork = json_array_get(jnetworks, i)) == NULL)
+			continue;
+
+		if ((n = malloc(sizeof(struct network))) == NULL)
+			return (-1);
+
+		json_unpack(jnetwork, "{s:s, s:s, s:s, s:s}", "name", n->name,
+		    "cert", n->cert, "pvkey", n->pvkey, "cacert", n->cacert);
+		n->idx = i;
+
+		RB_INSERT(network_tree, &networks, n);
+	}
 
 	return (0);
+}
+
+int
+ndb_network_add(const char *network_name, const char *pvkey,
+    const char *cert, const char *cacert)
+{
+
+	struct network	*n;
+
+	if ((n = malloc(sizeof(struct network))) == NULL) {
+		fprintf(stderr, "%s: malloc\n", __func__);
+		return -1;
+	}
+
+	n->name = strdup(network_name);
+	n->pvkey = strdup(pvkey);
+	n->cert = strdup(cert);
+	n->cacert = strdup(cacert);
+
+	RB_INSERT(network_tree, &networks, n);
+
+	ndb_save();
+
+	return (0);
+}
+
+int
+ndb_network_remove(const char *network_name)
+{
+	return (0);
+}
+
+int
+ndb_save()
+{
+	json_t		*jdb = NULL;
+	json_t		*jnetworks = NULL;
+	json_t		*jnetwork = NULL;
+	struct network	*n;
+	int		 ret = -1;
+
+	if ((jdb = json_object()) == NULL) {
+		fprintf(stderr, "%s: json_object\n", __func__);
+		goto out;
+	}
+
+	if ((jnetworks = json_array()) == NULL) {
+		fprintf(stderr, "%s: json_array\n", __func__);
+		goto out;
+	}
+
+	if (json_object_set_new_nocheck(jdb, "version",
+	    json_string(NDB_VERSION)) < 0 ||
+	    json_object_set_new_nocheck(jdb, "networks", jnetworks) < 0) {
+		fprintf(stderr, "%s: json_object_set_new_nocheck\n", __func__);
+		goto out;
+	}
+
+	RB_FOREACH(n, network_tree, &networks) {
+
+		if ((jnetwork = json_object()) == NULL) {
+			fprintf(stderr, "%s: json_object\n", __func__);
+			goto out;
+		}
+
+		if ((json_array_append(jnetworks, jnetwork)) < 0) {
+			fprintf(stderr, "%s: json_array_append\n", __func__);
+			goto out;
+		}
+
+		if (json_object_set_new_nocheck(jnetwork, "name",
+		    json_string(n->name)) < 0 ||
+		    json_object_set_new_nocheck(jnetwork, "pvkey",
+		    json_string(n->pvkey)) < 0 ||
+		    json_object_set_new_nocheck(jnetwork, "cert",
+		    json_string(n->cert)) < 0 ||
+		    json_object_set_new_nocheck(jnetwork, "cacert",
+		    json_string(n->cacert)) < 0) {
+			fprintf(stderr, "%s: json_object_set_new_nocheck\n",
+			    __func__);
+				goto out;
+		}
+	}
+
+	if (json_dump_file(jdb, ndb_path, JSON_INDENT(2)) < 0) {
+		fprintf(stderr, "%s: json_dump_file\n", __func__);
+		goto error;
+	}
+
+	ret = 0;
+
+out:
+	json_decref(jdb);
+
+	return (ret);
 }
 
 void
@@ -135,3 +252,5 @@ ndb_fini()
 {
 
 }
+
+RB_GENERATE_STATIC(network_tree, network, entry, network_cmp);
