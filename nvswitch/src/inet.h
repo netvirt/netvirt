a1e80cd09fc28043ea0fdee79fd8c63c4fd5ffce
Nicolas J. Bouliane <nicboul@gmail.com>
2017-06-07 21:44:45 -0400

    update inet subsystem
    keep only functions that are usefull for now
    clean the code

--- nvswitch/src/inet.h
+++ nvswitch/src/inet.h
@@ -1,7 +1,7 @@
 /*
  * NetVirt - Network Virtualization Platform
- * Copyright (C) 2009-2016
- * Nicolas J. Bouliane <admin@netvirt.org>
+ * Copyright (C) 2009-2017 Mind4Networks inc.
+ * Nicolas J. Bouliane <nib@m4nt.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -20,21 +20,8 @@
 #define ADDR_BROADCAST	0x2
 #define ADDR_MULTICAST	0x4
 
-const uint8_t mac_addr_broadcast[6];
-const uint8_t mac_ddr_empty[6];
-
-int inet_get_mac_addr_type(uint8_t *);
-int inet_get_mac_addr_dst(void *, uint8_t *);
-int inet_get_mac_addr_src(void *, uint8_t *);
-
-uint16_t inet_get_iphdr_len(void *);
-void inet_print_iphdr(void *);
-int inet_is_ipv4(void *);
-size_t inet_get_ipv4(void *, char *);
-int inet_is_ipv6(void *);
-
-void inet_print_ether_type(void *);
-void inet_print_ethernet(void *);
-//void inet_print_arp(peer_t *);
+void	inet_macaddr_dst(void *, uint8_t *);
+void	inet_macaddr_src(void *, uint8_t *);
+void	inet_print_addr(void *);
 
 #endif
