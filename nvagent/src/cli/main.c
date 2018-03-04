b0afc29bdd3c8bf68acb62f664f71da8ba9e9cbc
Nicolas Bouliane <nbouliane@digitalocean.com>
2018-03-04 15:07:29 -0500

    test reconnect

--- nvagent/src/cli/main.c
+++ nvagent/src/cli/main.c
@@ -91,6 +91,11 @@ main(int argc, char *argv[])
 	evthread_use_pthreads();
 #endif
 
+	if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
+		fprintf(stderr, "%s: signal", __func__);
+		exit(-1);
+	}
+
 	if (ndb_init() < 0) {
 		fprintf(stderr, "%s: db_init\n", __func__);
 		exit(-1);
