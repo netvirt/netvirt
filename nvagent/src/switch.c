53d9a5e0f545781c9373654fe2b691dd87f2c3d2
Nicolas J. Bouliane <nicboul@gmail.com>
2018-03-11 21:57:41 +0000

    first pass at refactoring switch to add reconnect capability

--- nvagent/src/switch.c
+++ nvagent/src/switch.c
@@ -50,6 +50,7 @@
 #include <jansson.h>
 
 #include <pki.h>
+#include <log.h>
 #include <tapcfg.h>
 
 #include "agent.h"
@@ -59,14 +60,28 @@ enum dtls_state {
 	DTLS_ESTABLISHED
 };
 
-struct dtls_peer {
+struct dtls_conn {
+	struct event	*ev_udpclient;
 	struct event	*handshake_timer;
 	struct event	*ping_timer;
 	enum dtls_state	 state;
 	SSL		*ssl;
-	tapcfg_t	*tapcfg;
-	int		 tapfd;
+	SSL_CTX		*ctx;
 	int		 sock;
+};
+
+enum vlink_state {
+	VSWITCH_DISCONNECTED,
+	VSWITCH_CONNECTED
+};
+
+struct vlink {
+	passport_t		*passport;
+	tapcfg_t		*tapcfg;
+	struct dtls_conn	*conn;
+	int			 tapfd;
+	char			*addr;
+	enum vlink_state	 state;
 };
 
 struct eth_hdr {
@@ -81,26 +96,23 @@ struct packets {
 	TAILQ_ENTRY(packets)	 entries;
 };
 
-static SSL_CTX			*ctx;
-static passport_t		*passport;
 static struct addrinfo		*ai;
 struct event_base		*ev_base;
-struct dtls_peer		 switch_peer;
 struct eth_hdr			 eth_ping;
-#if defined(_WIN32) || defined(__APPLE__)
 pthread_mutex_t			 mutex;
-#endif
 struct event			*ev_iface;
 
 TAILQ_HEAD(tailhead, packets)	 tailq_head;
 
-static int	 certverify_cb(int, X509_STORE_CTX *);
-static void	 dtls_peer_free(struct dtls_peer *);
-static void	 dtls_handshake_timeout_cb(int, short, void *);
-static int	 dtls_handle(struct dtls_peer *);
-static void	 iface_cb(int, short, void *);
-static void	 udpclient_cb(int, short, void *);
+static int	 	 certverify_cb(int, X509_STORE_CTX *);
+static void	 	 dtls_conn_free(struct dtls_conn *);
+static struct dtls_conn	*dtls_conn_new(struct vlink *);
+static void	 	 dtls_handshake_timeout_cb(int, short, void *);
+static int	 	 dtls_handle(struct vlink *);
+static void	 	 udpclient_cb(int, short, void *);
 
+static void		 vlink_reconnect(struct vlink *);
+static void	 	 iface_cb(int, short, void *);
 
 int
 certverify_cb(int ok, X509_STORE_CTX *store)
@@ -116,12 +128,6 @@ certverify_cb(int ok, X509_STORE_CTX *store)
 	printf("CN: %s\n", buf);
 
 	return (ok);
-}
-
-void
-dtls_peer_free(struct dtls_peer *p)
-{
-	(void)p;
 }
 
 void
@@ -141,19 +147,23 @@ info_cb(const SSL *ssl, int where, int ret)
 {
 	(void)ret;
 
-	struct dtls_peer	*p;
-	struct timeval		 tv;
+	struct vlink	*vlink;
+	struct timeval	 timeout = {5, 0};
 
 	if ((where & SSL_CB_HANDSHAKE_DONE) == 0)
 		return;
 
-	p = SSL_get_app_data(ssl);
+	vlink = SSL_get_app_data(ssl);
 
-	tv.tv_sec = 5;
-	tv.tv_usec = 0;
-	p->ping_timer = event_new(ev_base, -1, EV_TIMEOUT | EV_PERSIST,
+	vlink->conn->ping_timer = event_new(ev_base, -1, EV_TIMEOUT | EV_PERSIST,
 	    ping_timeout_cb, (SSL *)ssl);
-	event_add(p->ping_timer, &tv);
+	event_add(vlink->conn->ping_timer, &timeout);
+
+	// Reset the ev_udpclient event to remove the EV_TIMEOUT.
+	evtimer_del(vlink->conn->ev_udpclient);
+	event_assign(vlink->conn->ev_udpclient, ev_base, vlink->conn->sock,
+	    EV_READ | EV_PERSIST, udpclient_cb, vlink);
+	evtimer_add(vlink->conn->ev_udpclient, NULL);
 
 	printf("connected !\n");
 }
@@ -164,222 +174,23 @@ dtls_handshake_timeout_cb(int fd, short event, void *arg)
 	(void)fd;
 	(void)event;
 
-	struct dtls_peer	*p = arg;
+	struct vlink	*vlink = arg;
 
-	DTLSv1_handle_timeout(p->ssl);
+	DTLSv1_handle_timeout(vlink->conn->ssl);
 
-	if (dtls_handle(p) < 0) {
-		dtls_peer_free(p);
+	if (dtls_handle(vlink) < 0) {
+		vlink_reconnect(vlink);
 	}
-}
-
-int
-dtls_handle(struct dtls_peer *p)
-{
-	printf("dtls handle\n");
-	struct timeval	tv;
-	enum dtls_state	next_state;
-	int		ret;
-	char		buf[5000] = {0};
-
-	for (;;) {
-
-		switch (p->state) {
-		case DTLS_CONNECT:
-			ret = SSL_do_handshake(p->ssl);
-			next_state = DTLS_ESTABLISHED;
-			break;
-
-		case DTLS_ESTABLISHED:
-			ret = SSL_read(p->ssl, buf, sizeof(buf));
-			// XXX check ret
-			next_state = DTLS_ESTABLISHED;
-			if (ret > 0) {
-				ret = tapcfg_write(p->tapcfg, buf, ret);
-				// XXX check ret
-				return (0);
-			}
-			break;
-
-		default:
-			fprintf(stderr, "%s: invalid DTLS peer state\n", __func__);
-			return (-1);
-		}
-
-		switch (SSL_get_error(p->ssl, ret)) {
-		case SSL_ERROR_NONE:
-			break;
-
-		case SSL_ERROR_WANT_WRITE:
-			return (0);
-
-		case SSL_ERROR_WANT_READ:
-			if (DTLSv1_get_timeout(p->ssl, &tv) == 1 &&
-			    evtimer_add(p->handshake_timer, &tv) < 0) {
-				return (-1);
-			}
-			return (0);
-
-		default:
-			fprintf(stderr, "%s: ssl error\n", __func__);
-			return (-1);
-		}
-
-		p->state = next_state;
-	}
-
-	return (0);
-}
-
-void
-iface_cb(int sock, short what, void *arg)
-{
-	(void)sock;
-	(void)what;
-
-	struct dtls_peer	*p = arg;
-	int			 ret;
-	uint8_t			 buf[5000] = {0};
-
-	p = arg;
-
-	printf("iface cb\n");
-	ret = tapcfg_read(p->tapcfg, buf, sizeof(buf));
-	// XXX check ret
-
-	if (p->state == DTLS_ESTABLISHED) {
-		ret = SSL_write(p->ssl, buf, ret);
-		// XXX check ret
-	}
-}
-
-void
-udpclient_cb(int sock, short what, void *arg)
-{
-	(void)sock;
-	(void)what;
-	(void)arg;
-
-	struct dtls_peer	*p = arg;
-
-	if (what&EV_TIMEOUT || dtls_handle(p) < 0)
-		goto error;
-
-	return;
-
-error:
-	dtls_peer_free(p);
-}
-
-int
-switch_connect(const char *vswitch_addr, const char *network_name)
-{
-	BIO			*bio = NULL;
-	EC_KEY			*ecdh;
-	struct timeval		timeout = {5, 0};
-	struct event		*ev_udpclient;
-	struct addrinfo	 	hints;
-	struct dtls_peer	*p;
-	struct network		*netcf;
-	int			 status;
-	int			 flag;
-	int			 ret;
-	const char		*port = "9090";
-
-	printf("Connecting...\n");
-
-	p = &switch_peer;
-	p->state = DTLS_CONNECT;
-	p->handshake_timer = evtimer_new(ev_base, dtls_handshake_timeout_cb, p);
-
-	if ((netcf = ndb_network(network_name)) == NULL) {
-		fprintf(stderr, "%s: The network specified doesn't exist: %s\n",
-		    __func__, network_name);
-		return (-1);
-	}
-
-	if ((passport = pki_passport_load_from_memory(netcf->cert, netcf->pvkey, netcf->cacert))
-	    == NULL)
-		err(1, "%s: pki_passport_load_from_memory", __func__);
-
-	SSL_library_init();
-	SSL_load_error_strings();
-
-	if (!RAND_poll())
-		err(1, "%s: RAND_poll", __func__);
-
-	if ((ctx = SSL_CTX_new(DTLSv1_client_method())) == NULL)
-		errx(1, "%s: SSL_CTX_new", __func__);
-
-	SSL_CTX_set_read_ahead(ctx, 1);
-
-	SSL_CTX_set_cert_store(ctx, passport->cacert_store);
-	SSL_CTX_use_certificate(ctx, passport->certificate);
-	SSL_CTX_use_PrivateKey(ctx, passport->keyring);
-
-	if ((ret = SSL_CTX_set_cipher_list(ctx, "ECDHE-ECDSA-AES256-SHA")) == 0)
-		err(1, "%s: SSL_CTX_set_cipher_list", __func__);
-
-	if ((ecdh = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1)) == NULL)
-		err(1, "%s: EC_KEY_new_by_curve_name", __func__);
-
-	SSL_CTX_set_tmp_ecdh(ctx, ecdh);
-	EC_KEY_free(ecdh);
-
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_DGRAM;
-
-	if ((status = getaddrinfo(vswitch_addr, port, &hints, &ai)) != 0)
-		errx(1, "%s: getaddrinfo %s", gai_strerror(status), __func__);
-
-	if ((p->sock = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol)) < 0)
-		errx(1, "%s: socket", __func__);
-
-#ifndef WIN32
-	flag = 1;
-	if (setsockopt(p->sock, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag)) < 0)
-		errx(1, "%s: setsockopt", __func__);
-#endif
-
-	if (connect(p->sock, ai->ai_addr, ai->ai_addrlen) < 0)
-		warn("%s: connect", __func__);
-
-	if ((p->ssl = SSL_new(ctx)) == NULL || SSL_set_app_data(p->ssl, p) != 1)
-		warnx("%s: SSL_new", __func__);
-
-	SSL_set_info_callback(p->ssl, info_cb);
-	SSL_set_tlsext_host_name(p->ssl, passport->certinfo->network_uid);
-	SSL_set_verify(p->ssl,
-	    SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT, certverify_cb);
-
-	if ((bio = BIO_new_dgram(p->sock, BIO_NOCLOSE)) == NULL)
-		warnx("%s: BIO_new_dgram", __func__);
-
-	BIO_ctrl(bio, BIO_CTRL_DGRAM_SET_CONNECTED, 0, &ai->ai_addr);
-
-	SSL_set_bio(p->ssl, bio, bio);
-
-	if (evutil_make_socket_nonblocking(p->sock) > 0)
-		err(1, "%s: evutil_make_socket_nonblocking", __func__);
-
-	SSL_set_connect_state(p->ssl);
-	SSL_connect(p->ssl);
-
-	if ((ev_udpclient = event_new(ev_base, p->sock,
-	    EV_READ|EV_TIMEOUT|EV_PERSIST, udpclient_cb, p)) == NULL)
-		warn("%s: event_new", __func__);
-	event_add(ev_udpclient, &timeout);
-
-	return (0);
 }
 
 #if defined(_WIN32) || defined(__APPLE__)
 void
-cb_test(int sock, short what, void *arg)
+iface_cb(int sock, short what, void *arg)
 {
 	struct packets		 *pkt;
-	struct dtls_peer	 *p = arg;
+	struct dtls_conn	 *conn = arg;
+	(void)sock;
+	(void)what;
 
 	pthread_mutex_lock(&mutex);
 	while ((pkt = TAILQ_FIRST(&tailq_head)) == NULL) {
@@ -388,8 +199,8 @@ cb_test(int sock, short what, void *arg)
 	}
 	pthread_mutex_unlock(&mutex);
 
-	if (p->state == DTLS_ESTABLISHED) {
-		SSL_write(p->ssl, pkt->buf, pkt->len);
+	if (conn->state == DTLS_ESTABLISHED) {
+		SSL_write(conn->ssl, pkt->buf, pkt->len);
 		// XXX check ret
 	}
 
@@ -400,14 +211,15 @@ cb_test(int sock, short what, void *arg)
 
 void *poke_tap(void *arg)
 {
-	struct dtls_peer	*p = arg;
-	struct packets		 *pkt;
+	struct vlink	*vlink = arg;
+	struct packets	*pkt;
 
+	// XXX
 	pkt = malloc(sizeof(struct packets));
 
 	while (1) { // XXX add circuit-breaker
 
-		pkt->len = tapcfg_read(p->tapcfg, pkt->buf, sizeof(pkt->buf));
+		pkt->len = tapcfg_read(vlink->tapcfg, pkt->buf, sizeof(pkt->buf));
 		// XXX check len
 
 		pthread_mutex_lock(&mutex);
@@ -418,45 +230,399 @@ void *poke_tap(void *arg)
 
 	return (NULL);
 }
+#else
+void
+iface_cb(int sock, short what, void *arg)
+{
+	(void)sock;
+	(void)what;
+
+	struct vlink	*vlink = arg;
+	int			 ret;
+	uint8_t			 buf[5000] = {0};
+
+	vlink = arg;
+
+	ret = tapcfg_read(vlink->tapcfg, buf, sizeof(buf));
+	// XXX check ret
+
+	if (vlink->conn != NULL && vlink->conn->state == DTLS_ESTABLISHED) {
+		ret = SSL_write(vlink->conn->ssl, buf, ret);
+		// XXX check ret
+	}
+}
 #endif
+
+int
+dtls_handle(struct vlink *vlink)
+{
+	struct timeval	tv;
+	enum dtls_state	next_state;
+	int		ret;
+	char		buf[5000] = {0};
+
+	for (;;) {
+
+		switch (vlink->conn->state) {
+		case DTLS_CONNECT:
+			ret = SSL_do_handshake(vlink->conn->ssl);
+			next_state = DTLS_ESTABLISHED;
+			break;
+
+		case DTLS_ESTABLISHED:
+			ret = SSL_read(vlink->conn->ssl, buf, sizeof(buf));
+			// XXX check ret
+			next_state = DTLS_ESTABLISHED;
+			if (ret > 0) {
+				ret = tapcfg_write(vlink->tapcfg, buf, ret);
+				// XXX check ret
+				return (0);
+			}
+			break;
+
+		default:
+			fprintf(stderr, "%s: invalid DTLS peer state\n", __func__);
+			return (-1);
+		}
+
+		switch (SSL_get_error(vlink->conn->ssl, ret)) {
+		case SSL_ERROR_NONE:
+			break;
+
+		case SSL_ERROR_WANT_WRITE:
+			return (0);
+
+		case SSL_ERROR_WANT_READ:
+			if (DTLSv1_get_timeout(vlink->conn->ssl, &tv) == 1 &&
+			    evtimer_add(vlink->conn->handshake_timer, &tv) < 0) {
+				return (-1);
+			}
+			return (0);
+
+		default:
+			fprintf(stderr, "%s: ssl error\n", __func__);
+			return (-1);
+		}
+
+		vlink->conn->state = next_state;
+	}
+
+	return (0);
+}
+
+void
+udpclient_cb(int sock, short what, void *arg)
+{
+	(void)sock;
+	(void)what;
+	(void)arg;
+
+	struct vlink	*vlink = arg;
+
+	printf("udpclient_cb\n");
+
+	if (what & EV_TIMEOUT) {
+		printf("sock timeout !\n");
+		goto error;
+	}
+
+	if (dtls_handle(vlink) < 0) {
+		printf("dtls handle failed\n");
+		goto error;
+	}
+
+	return;
+
+error:
+	vlink_reconnect(vlink);
+	return;
+}
+
+void
+dtls_conn_free(struct dtls_conn *conn)
+{
+	if (conn == NULL)
+		return;
+
+	if (conn->ssl != NULL) {
+		SSL_set_shutdown(conn->ssl, SSL_RECEIVED_SHUTDOWN);
+		SSL_shutdown(conn->ssl);
+		SSL_free(conn->ssl);
+	}
+
+	close(conn->sock);
+
+	if (conn->ctx != NULL)
+		SSL_CTX_free(conn->ctx);
+
+	if (conn->handshake_timer != NULL)
+		event_del(conn->handshake_timer);
+
+	if (conn->ping_timer != NULL)
+		event_del(conn->ping_timer);
+
+	if (conn->ev_udpclient != NULL)
+		evtimer_del(conn->ev_udpclient);
+
+	free(conn);
+}
+
+struct dtls_conn *
+dtls_conn_new(struct vlink *vlink)
+{
+	BIO			*bio = NULL;
+	EC_KEY			*ecdh;
+	struct timeval		timeout = {5, 0};
+	struct addrinfo	 	hints;
+	struct dtls_conn	*conn;
+	unsigned long		 e;
+	int			 flag;
+	int			 ret;
+	const char		*port = "9090";
+
+	// XXX global init! 
+	{
+		SSL_library_init();
+		SSL_load_error_strings();
+		if (!RAND_poll())
+			err(1, "%s: RAND_poll", __func__);
+	}
+
+	printf("Connecting to %s\n", vlink->addr);
+
+	if ((conn = malloc(sizeof(struct dtls_conn))) == NULL) {
+		log_warn("%s: malloc", __func__);
+		goto cleanup;
+	}
+	conn->handshake_timer = NULL;
+	conn->ping_timer = NULL;
+	conn->state = DTLS_CONNECT;
+	conn->ssl = NULL;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	if ((ret = getaddrinfo(vlink->addr, port, &hints, &ai)) < 0) {
+		log_warnx("%s: getaddrinfo %s", __func__, gai_strerror(ret));
+		goto cleanup;
+	}
+
+	if ((conn->sock = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol)) < 0) {
+		log_warnx("%s: socket", __func__);
+		goto cleanup;
+	}
+
+#ifndef WIN32
+	flag = 1;
+	if (setsockopt(conn->sock, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag)) < 0) {
+		log_warn("%s: setsockopt", __func__);
+		goto cleanup;
+	}
+#endif
+
+	if (evutil_make_socket_nonblocking(conn->sock) > 0) {
+		log_warnx("%s: evutil_make_socket_nonblocking", __func__);
+		goto cleanup;
+	}
+
+	if (connect(conn->sock, ai->ai_addr, ai->ai_addrlen) < 0) {
+		log_warn("%s: connect", __func__);
+		goto cleanup;
+	}
+
+	if ((conn->ctx = SSL_CTX_new(DTLSv1_client_method())) == NULL) {
+		log_warnx("%s: SSL_CTX_new", __func__);
+		while ((e = ERR_get_error()))
+			log_warnx("%s", ERR_error_string(e, NULL));
+		goto cleanup;
+	}
+
+	if (SSL_CTX_set_cipher_list(conn->ctx, "ECDHE-ECDSA-AES256-SHA") != 1) {
+		log_warnx("%s: SSL_CTX_set_cipher_list", __func__);
+		while ((e = ERR_get_error()))
+			log_warnx("%s", ERR_error_string(e, NULL));
+		goto cleanup;
+	}
+
+	if ((ecdh = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1)) == NULL) {
+		log_warnx("%s: EC_KEY_new_by_curve_name", __func__);
+		goto cleanup;
+	}
+	SSL_CTX_set_tmp_ecdh(conn->ctx, ecdh);
+	EC_KEY_free(ecdh);
+
+
+	SSL_CTX_set_cert_store(conn->ctx, vlink->passport->cacert_store);
+	X509_STORE_up_ref(vlink->passport->cacert_store);
+
+	if ((SSL_CTX_use_certificate(conn->ctx, vlink->passport->certificate)) != 1) {
+		log_warnx("%s: SSL_CTX_use_certificate", __func__);
+		goto cleanup;
+	}
+	if ((SSL_CTX_use_PrivateKey(conn->ctx, vlink->passport->keyring)) != 1) {
+		log_warnx("%s: SSL_CTX_use_PrivateKey", __func__);
+		goto cleanup;
+	}
+
+	SSL_CTX_set_read_ahead(conn->ctx, 1);
+
+	if ((conn->ssl = SSL_new(conn->ctx)) == NULL) {
+		log_warnx("%s: SSL_new", __func__);
+		goto cleanup;
+	}
+
+	if (SSL_set_app_data(conn->ssl, vlink) != 1) {
+		log_warnx("%s: SSL_set_app_data", __func__);
+		goto cleanup;
+	}
+
+	SSL_set_info_callback(conn->ssl, info_cb);
+	SSL_set_tlsext_host_name(conn->ssl, vlink->passport->certinfo->network_uid);
+	SSL_set_verify(conn->ssl,
+	    SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT, certverify_cb);
+
+	if ((bio = BIO_new_dgram(conn->sock, BIO_NOCLOSE)) == NULL) {
+		log_warnx("%s: BIO_new_dgram", __func__);
+		goto cleanup;
+	}
+
+	BIO_ctrl(bio, BIO_CTRL_DGRAM_SET_CONNECTED, 0, &ai->ai_addr);
+
+	SSL_set_bio(conn->ssl, bio, bio);
+
+	SSL_set_connect_state(conn->ssl);
+
+	if ((conn->ev_udpclient = event_new(ev_base, conn->sock,
+	    EV_READ|EV_PERSIST, udpclient_cb, vlink)) == NULL) {
+		log_warnx("%s: event_new", __func__);
+		goto cleanup;
+	}
+	event_add(conn->ev_udpclient, &timeout);
+
+	if ((conn->handshake_timer = evtimer_new(ev_base, dtls_handshake_timeout_cb, vlink)) == NULL) {
+		log_warnx("%s: evtimer_new", __func__);
+		goto cleanup;
+	}
+
+	SSL_connect(conn->ssl);
+
+	return (conn);
+
+cleanup:
+
+	vlink_reconnect(vlink);
+	return (NULL);
+}
+
+void
+vlink_connstart(evutil_socket_t fd, short what, void *arg)
+{
+	struct vlink	*vlink = arg;
+	(void)fd;
+	(void)what;
+
+	if (vlink->conn != NULL) {
+		dtls_conn_free(vlink->conn);
+		vlink->conn = NULL;
+	}
+
+	if ((vlink->conn = dtls_conn_new(vlink)) == NULL) {
+		log_warnx("%s: dtls_conn_new", __func__);
+		goto error;
+	}
+
+	return;
+
+error:
+	vlink_reconnect(vlink);
+
+	return;
+}
+
+void
+vlink_reconnect(struct vlink *vlink)
+{
+	struct timeval	one_sec = {5, 0};
+
+	vlink->state = VSWITCH_DISCONNECTED;
+
+	// Disable timeout on the UDP socket to prevent calling reconnect again.
+	if (vlink->conn != NULL && vlink->conn->ev_udpclient != NULL)
+		evtimer_del(vlink->conn->ev_udpclient);
+
+	if (event_base_once(ev_base, -1, EV_TIMEOUT,
+	    vlink_connstart, vlink, &one_sec) < 0)
+		log_warnx("%s: event_base_once", __func__);
+}
 
 int
 switch_init(tapcfg_t *tapcfg, int tapfd, const char *vswitch_addr, const char *ipaddr,
     const char *network_name)
 {
-	struct dtls_peer	*p;
-
-	TAILQ_INIT(&tailq_head);
+	struct network	*netcf = NULL;
+	struct vlink	*vlink = NULL;
 
 	eth_ping.ethertype = htons(0x9000);
-	p = &switch_peer;
 
-	p->tapcfg = tapcfg;
-	p->tapfd = tapfd;
+	if ((vlink = malloc(sizeof(struct vlink))) == NULL) {
+		log_warn("%s: malloc", __func__);
+		goto cleanup;
+	}
+	vlink->passport = NULL;
+	vlink->tapcfg = NULL;
+	vlink->conn = NULL;
+	vlink->addr = NULL;
+
+	vlink->tapcfg = tapcfg;
+	vlink->tapfd = tapfd;
+
+	if ((vlink->addr = strdup(vswitch_addr)) == NULL) {
+		log_warn("%s: strdup", __func__);
+		goto cleanup;
+	}
 
 	tapcfg_iface_set_status(tapcfg, TAPCFG_STATUS_IPV4_UP);
+	// XXX netmask not always 24
 	tapcfg_iface_set_ipv4(tapcfg, ipaddr, 24);
 
+	if ((netcf = ndb_network(network_name)) == NULL) {
+		log_warnx("%s: the network doesn't exist: %s",
+		    __func__, network_name);
+		goto cleanup;
+	}
+
+	if ((vlink->passport =
+	    pki_passport_load_from_memory(netcf->cert, netcf->pvkey, netcf->cacert)) == NULL) {
+		log_warnx("%s: pki_passport_load_from_memory", __func__);
+		goto cleanup;
+	}
+
 #if defined(_WIN32) || defined(__APPLE__)
+	TAILQ_INIT(&tailq_head);
+
 	pthread_t thread_poke_tap;
 	pthread_attr_t	attr;
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-	pthread_create(&thread_poke_tap, &attr, poke_tap, (void *)p);
+	pthread_create(&thread_poke_tap, &attr, poke_tap, (void *)vlink);
 
-	if ((ev_iface = event_new(ev_base, 0, EV_TIMEOUT, cb_test, p)) == NULL)
+	if ((ev_iface = event_new(ev_base, 0, EV_TIMEOUT, iface_cb, vlink)) == NULL)
 		warn("%s:%d", "event_new", __LINE__);
 	event_add(ev_iface, NULL);
 #else
 	if ((ev_iface = event_new(ev_base, tapfd,
-	    EV_READ | EV_PERSIST, iface_cb, p)) == NULL)
+	    EV_READ | EV_PERSIST, iface_cb, vlink)) == NULL)
 		warn("%s:%d", "event_new", __LINE__);
 	event_add(ev_iface, NULL);
 #endif
 
-	switch_connect(vswitch_addr, network_name);
+	vlink_reconnect(vlink);
 
-	return (0);	
+	return (0);
+
+cleanup:
+
+	return (-1);
 }
 
 void
